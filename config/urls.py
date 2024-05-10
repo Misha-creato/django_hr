62d00f1912500813e5ae90f217ad6fceb3308ac6
Iri O <oparina.iri@gmail.com>
2024-05-10 19:44:23 +0500

    Added plugin config url

--- config/urls.py
+++ config/urls.py
@@ -24,6 +24,7 @@ from django.urls import (
 
 
 urlpatterns = [
+    path("ckeditor5/", include('django_ckeditor_5.urls')),
     path('admin/', admin.site.urls),
     path('', include('index.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
