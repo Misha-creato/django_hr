13b3407f62ecd07cc4eaf65883d1bf7ff26e27d5
Iri O <oparina.iri@gmail.com>
2024-05-10 19:30:48 +0500

    Changed index view

--- apps/index/views.py
+++ apps/index/views.py
@@ -1,29 +1,12 @@
 from django.shortcuts import render
 from django.views import View
 
-from headers.models import Header
-from vacancies.models import (
-    Vacancy,
-    Company,
-)
+from index.services import get_context_data
 
 
 class IndexView(View):
     def get(self, request, *args, **kwargs):
-        header = Header.objects.first()
-        experience = Vacancy.EXPERIENCE_CHOICES
-        employment = Vacancy.EMPLOYMENT_CHOICES
-        schedule = Vacancy.SCHEDULE_CHOICES
-        vacancies = Vacancy.objects.filter(company__hidden=False, hidden=False)
-        companies = Company.objects.filter(hidden=False)
-        context = {
-            'header': header,
-            'EXPERIENCE_CHOICES': experience,
-            'EMPLOYMENT_CHOICES': employment,
-            'SCHEDULE_CHOICES': schedule,
-            'vacancies': vacancies,
-            'companies': companies,
-        }
+        context = get_context_data()
         return render(
             request=request,
             template_name='index.html',
