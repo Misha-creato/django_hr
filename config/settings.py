46eeec525565ca66359e1da07b0680b5a12d0995
Iri O <oparina.iri@gmail.com>
2024-05-14 08:10:57 +0500

    Added fixture settings

--- config/settings.py
+++ config/settings.py
@@ -149,11 +149,18 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
 # Media
 
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# fixtures
+
+FIXTURE_DIRS = (
+    os.path.join(BASE_DIR, 'apps', 'index', 'tests', 'fixtures'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
