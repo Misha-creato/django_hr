c38c4b87352801a4cc4ecc6d77ca9f090e4d10c7
Iri O <oparina.iri@gmail.com>
2024-05-16 18:06:15 +0500

    Changed response data, deleted data_and_files check

--- apps/index/check.py
+++ apps/index/check.py
@@ -6,41 +6,29 @@ from django.core.validators import validate_email
 from utils.constants import RESUME_VALID_EXTENSIONS
 
 
-# def required_data(data):
-#     keys = [
-#         'email',
-#     ]
-#     for key in keys:
-#         if not data.get(key):
-#             print(f'Отсутствие ключа {key}')
-#             return False
-#     return True
-#
-
-# def required_files(files):
-#     keys = [
-#         'resume',
-#     ]
-#     for key in keys:
-#         if not files.get(key):
-#             print(f'Отсутствие файла {key}')
-#             return False
-#     return True
-
-
-def required_data_and_files(data, files):
+def required_data(data) -> bool:
     keys = [
-        ('email', data),
-        ('resume', files),
+        'email',
     ]
-    for key, source in keys:
-        if not source.get(key):
-            print(f'Отсутствует {key}')
+    for key in keys:
+        if not data.get(key):
+            print(f'Отсутствие ключа {key}')
             return False
     return True
 
 
-def is_email_valid(email):
+def required_files(files) -> bool:
+    keys = [
+        'resume',
+    ]
+    for key in keys:
+        if not files.get(key):
+            print(f'Отсутствие файла {key}')
+            return False
+    return True
+
+
+def is_email_valid(email) -> bool:
     try:
         validate_email(email)
     except ValidationError as exc:
@@ -49,7 +37,7 @@ def is_email_valid(email):
     return True
 
 
-def is_file_extension_valid(file, extensions):
+def is_file_extension_valid(file, extensions) -> bool:
     file_extension = os.path.splitext(file.name)[1]
     if file_extension not in extensions:
         print(f'Расширение файла {file.name} не валидно')
@@ -57,27 +45,22 @@ def is_file_extension_valid(file, extensions):
     return True
 
 
-def response_data(data, files):
-    checks = [
-        (required_data_and_files,
-         {
-             'data': data,
-             'files': files,
-         }
-         ),
-        (is_email_valid,
-         {
-             'email': data.get('email'),
-         }
-         ),
-        (is_file_extension_valid,
-         {
-             'file': files.get('resume'),
-             'extensions': RESUME_VALID_EXTENSIONS,
-         }
-         ),
+def response_data(data, files) -> bool:
+    data_checks = [
+        required_data,
     ]
-    for check, arguments in checks:
-        if not check(**arguments):
+    files_checks = [
+        required_files,
+    ]
+    for data_check, file_check in zip(data_checks, files_checks):
+        if not data_check(data):
             return False
+        if not file_check(files):
+            return False
+
+    if not is_email_valid(data['email']):
+        return False
+    if not is_file_extension_valid(files['resume'], RESUME_VALID_EXTENSIONS):
+        return False
+
     return True
